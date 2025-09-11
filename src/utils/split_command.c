bd630d9e1e792101025e984905927e35bd74d80a
Alelith <eacaymo@gmail.com>
2025-09-11 16:14:31 +0200

    split semi-fixed

--- src/utils/split_command.c
+++ src/utils/split_command.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   split_command.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bvarea-k <bvarea-k@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: acesteve <acesteve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 18:15:08 by acesteve          #+#    #+#             */
-/*   Updated: 2025/09/11 13:50:51 by bvarea-k         ###   ########.fr       */
+/*   Updated: 2025/09/11 16:05:20 by acesteve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	is_fromset(char c, char *set)
+static int	is_fromset(char c, const char *set)
 {
 	while (set && *set)
 	{
@@ -23,16 +23,24 @@ static int	is_fromset(char c, char *set)
 	return (0);
 }
 
-static char	*get_word(char *line, const char *delimiters)
+static char	*get_word(char *line, const char *delimiters, int *index)
 {
+	int	word_len;
 	
+	word_len = 0;
+	if (str_search_char(delimiters, '\n') == 0)
+		line++;
+	while (line[word_len] && !is_fromset(line[word_len], delimiters))
+		word_len++;
+	*index = *index + word_len;
+	return (str_substring(line, 0, word_len));
+		
 }
 
-//TODO: Si hay dos cadenas de literales seguidas sin espacio se toma como una sola cadena
+//TODO: Si hay dos cadenas seguidas sin espacio se toma como una sola cadena
 char	**split_command(char *line, t_env *env)
 {
-	const char **delimiters = {" \n\t\r", "'", "\""};
-	int		word_len;
+	const char *delimiters[5] = {" \n\t\r", "'", "\""};
 	int		argc;
 	char	*tmp;
 	char	**args;
@@ -46,7 +54,12 @@ char	**split_command(char *line, t_env *env)
 		if (line[i] && !is_space(line[i]))
 		{
 			args = reallocation(args, (argc + 1) * sizeof(char *), argc * sizeof(char *));
-			args[argc++] = get_word();	
+			if (line[i] == '"')
+				args[argc++] = get_word(&line[i], delimiters[2], &i);
+			else if (line[i] == '\'')
+				args[argc++] = get_word(&line[i], delimiters[1], &i);
+			else
+				args[argc++] = get_word(&line[i], delimiters[0], &i);
 		}
 		if (line[i])
 			i++;
