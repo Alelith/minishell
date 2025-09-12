e16015f0fa3dbd4f9589a23105ab520de2fd6959
Lilith Estévez Boeta <eacaymo@gmail.com>
2025-09-12 12:35:50 +0200

    Split working completly

--- src/utils/split_command.c
+++ src/utils/split_command.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split_command.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acesteve <acesteve@student.42.fr>          +#+  +:+       +#+        */
+/*   By: acesteve <acesteve@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 18:15:08 by acesteve          #+#    #+#             */
-/*   Updated: 2025/09/11 16:05:20 by acesteve         ###   ########.fr       */
+/*   Updated: 2025/09/12 12:34:10 by acesteve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,10 @@ static char	*get_word(char *line, const char *delimiters, int *index)
 	int	word_len;
 	
 	word_len = 0;
-	if (str_search_char(delimiters, '\n') == 0)
-		line++;
 	while (line[word_len] && !is_fromset(line[word_len], delimiters))
 		word_len++;
-	*index = *index + word_len;
+	*index = *index + word_len + 1;
+	//if (line[index] == )
 	return (str_substring(line, 0, word_len));
 		
 }
@@ -40,7 +39,6 @@ static char	*get_word(char *line, const char *delimiters, int *index)
 //TODO: Si hay dos cadenas seguidas sin espacio se toma como una sola cadena
 char	**split_command(char *line, t_env *env)
 {
-	const char *delimiters[5] = {" \n\t\r", "'", "\""};
 	int		argc;
 	char	*tmp;
 	char	**args;
@@ -55,13 +53,13 @@ char	**split_command(char *line, t_env *env)
 		{
 			args = reallocation(args, (argc + 1) * sizeof(char *), argc * sizeof(char *));
 			if (line[i] == '"')
-				args[argc++] = get_word(&line[i], delimiters[2], &i);
+				args[argc++] = get_word(&line[++i], "\"", &i);
 			else if (line[i] == '\'')
-				args[argc++] = get_word(&line[i], delimiters[1], &i);
+				args[argc++] = get_word(&line[++i], "'", &i);
 			else
-				args[argc++] = get_word(&line[i], delimiters[0], &i);
+				args[argc++] = get_word(&line[i], " \n\t\r", &i);
 		}
-		if (line[i])
+		else if (line[i])
 			i++;
 	}
 	args = reallocation(args, (argc + 1) * sizeof(char *), argc * sizeof(char *));
