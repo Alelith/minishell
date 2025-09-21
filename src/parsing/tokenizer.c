bab3555dde99eb10fcd3d19a28621e69e03cc9df
Lilith Estévez Boeta <eacaymo@gmail.com>
2025-09-21 12:37:26 +0200

    Only try_command doesn't pass norminette

--- src/parsing/tokenizer.c
+++ src/parsing/tokenizer.c
@@ -6,11 +6,35 @@
 /*   By: acesteve <acesteve@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 13:33:37 by bvarea-k          #+#    #+#             */
-/*   Updated: 2025/09/21 11:59:02 by acesteve         ###   ########.fr       */
+/*   Updated: 2025/09/21 12:36:01 by acesteve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	declare_vars_tokenize(int *i, char ***tokens, t_command **result)
+{
+	*i = 0;
+	*tokens = 0;
+	*result = 0;
+}
+
+static void	handle_pipe_token(char **tokens, int i, t_command **result,
+	unsigned short *len)
+{
+	free(tokens[i]);
+	*result = reallocation(*result, (*len + 1) * sizeof(t_command),
+			*len * sizeof(t_command));
+	(*result)[*len].outfile = 1;
+	*len = *len + 1;
+}
+
+static void	handle_redir_token(char **tokens, int *i, t_command **result,
+	unsigned short *len)
+{
+	proccess_redir(tokens, *i, &(*result)[*len - 1]);
+	*i = *i + 1;
+}
 
 t_command	*tokenize(char *input, unsigned short *len, t_shell shell)
 {
@@ -21,27 +45,19 @@ t_command	*tokenize(char *input, unsigned short *len, t_shell shell)
 	i = 0;
 	if (str_len(input) == 0)
 		return (0);
+	declare_vars_tokenize(&i, &tokens, &result);
 	result = init_command();
 	*len = 1;
 	tokens = get_tokens(input, shell);
 	while (tokens && tokens[i])
 	{
 		if (is_pipe_token(tokens[i]))
-		{
-			free(tokens[i]);
-			result = reallocation(result, (*len + 1) * sizeof(t_command),
-					*len * sizeof(t_command));
-			result[*len].outfile = 1;
-			*len = *len + 1;
-		}
+			handle_pipe_token(tokens, i, &result, len);
 		else if (is_redir_token(tokens[i]))
-		{
-			proccess_redir(tokens, i, &result[*len - 1]);
-			i = i + 1;
-		}
+			handle_redir_token(tokens, &i, &result, len);
 		else
 			proccess_command(tokens[i], &result[*len - 1]);
-		i = i + 1;
+		i++;
 	}
 	free(tokens);
 	return (result);
