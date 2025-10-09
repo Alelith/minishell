90aac5112e382cb60098653a429382eb6237feda
Lilith Estévez Boeta <eacaymo@gmail.com>
2025-10-09 19:17:10 +0200

    Quotes casi working

--- src/environment_vars/search_env.c
+++ src/environment_vars/search_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   search_env.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bvarea-k <bvarea-k@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: acesteve <acesteve@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 11:49:30 by bvarea-k          #+#    #+#             */
-/*   Updated: 2025/09/23 17:30:32 by bvarea-k         ###   ########.fr       */
+/*   Updated: 2025/10/09 19:15:32 by acesteve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ char	*search_env(t_env *envs, const char *key)
 	while (curr)
 	{
 		if (str_compare_n(curr->pair.key, key,
-				str_len(key) - str_len(str_search_char(key, ' '))))
+				str_len(key) - str_len(str_search_char(key, ' '))) ||
+			str_compare_n(curr->pair.key, key,
+				str_len(key) - str_len(str_search_char(key, '\''))))
 			return (curr->pair.value);
 		curr = curr->next;
 	}
